86d2e715ea37adab113536e9534ef691c55a9d85
Howard <xbgskg@yahoo.com.tw>
2017-07-19 00:31:29 +0800

    第一天作業

---
+++ SkillTreeMVCHomework/SkillTreeMVCHomework/Controllers/TallybookController.cs
@@ -0,0 +1,40 @@
+﻿using SkillTreeMVCHomework.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SkillTreeMVCHomework.Controllers
+{
+    public class TallybookController : Controller
+    {
+        // GET: Tallybook
+        public ActionResult Index()
+        {
+            Random rNumber = new Random();
+            var model = new TallybookViewModel();
+            model.spendingList = new List<Spending>();
+            for (int i = 0; i < 10; i++)
+            {
+                model.spendingList.Add(new Spending
+                {
+                    Category = rNumber.Next(0, 2) == 0 ? "收入" : "支出",
+                    Date = DateTime.Now.Date.AddDays(rNumber.Next(-7, -1)),
+                    Money = rNumber.Next(50, 300)*100
+                });
+            }
+            model.spendingList = model.spendingList.OrderBy(x => x.Date).ToList();
+            
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Index(TallybookViewModel pageData)
+        {
+            //新增資料
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ SkillTreeMVCHomework/SkillTreeMVCHomework/Models/Spending.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SkillTreeMVCHomework.Models
+{
+    public class Spending
+    {
+        [Required(ErrorMessage = "*請選擇類別")]
+        [Display(Name = "類別")]
+        public string Category { get; set; }
+        [Required(ErrorMessage = "*請輸入金額")]
+        [Display(Name = "金額")]
+        [DataType(DataType.Currency)]
+        public int Money { get; set; }
+        [Required(ErrorMessage = "*請輸入日期")]
+        [DisplayFormat(DataFormatString = "{0:d}")]
+        //[DataType(DataType.Date)]
+        [Display(Name = "日期")]
+        public DateTime Date { get; set; }
+        [Display(Name = "備註")]
+        public string Description { get; set; }
+    }
+}
